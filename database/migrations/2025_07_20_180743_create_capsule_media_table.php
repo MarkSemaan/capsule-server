cbc77e50143c823f4014330d4db25408cb255e44
MarkSemaan <mark.h.semaan@gmail.com>
2025-07-20 21:11:39 +0300

    implemented migrations for capsules and related media

---
+++ database/migrations/2025_07_20_180743_create_capsule_media_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('capsule_media', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('capsule_id')->constrained('capsules');
+            $table->enum('type', ['image', 'audio', 'text']);
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('capsule_media');
+    }
+};
